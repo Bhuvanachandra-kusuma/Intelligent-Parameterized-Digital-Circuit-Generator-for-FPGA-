d22be2964e2c6de28e76bdf6da213d90aae88f1e
Bhuvanachandra Kusuma <159415259+Bhuvanachandra-kusuma@users.noreply.github.com>
2026-04-10 02:12:24 +0200

    Refactor 4-bit adder to use ripple carry logic

--- generated/adder_4_rca.v
+++ generated/adder_4_rca.v
@@ -1,38 +1,10 @@
-module cla_4bit(
-    input [3:0] A, B,
-    input Cin,
-    output [3:0] SUM,
-    output Cout
-);
 
-    wire [3:0] P, G;
-    wire [4:0] C;
-
-    assign C[0] = Cin;
-
-    assign P = A ^ B;
-    assign G = A & B;
-
-    assign C[1] = G[0] | (P[0] & C[0]);
-    assign C[2] = G[1] | (P[1]&G[0]) | (P[1]&P[0]&C[0]);
-    assign C[3] = G[2] | (P[2]&G[1]) | (P[2]&P[1]&G[0]) | (P[2]&P[1]&P[0]&C[0]);
-    assign C[4] = G[3] | (P[3]&G[2]) | (P[3]&P[2]&G[1]) |
-                  (P[3]&P[2]&P[1]&G[0]) |
-                  (P[3]&P[2]&P[1]&P[0]&C[0]);
-
-    assign SUM = P ^ C[3:0];
-    assign Cout = C[4];
-
-endmodule
-
-
-module adder_4_cla(
-    input [3:0] A, B,
+module adder_4_rca(
+    input [3:0] A,
+    input [3:0] B,
     output [3:0] SUM
 );
 
-    wire Cout;
-
-    cla_4bit u0(A, B, 1'b0, SUM, Cout);
+assign SUM = A + B;
 
 endmodule
